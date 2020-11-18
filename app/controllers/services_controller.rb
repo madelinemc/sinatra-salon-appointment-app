acdc66a1169ae65a2ed7c4e4b9c0aa0e959cdcc1
madelinemc <madeline.coons@gmail.com>
2020-11-18 16:48:14 -0600

    added notes for services_controller routes and views

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,3 +1,13 @@
 class ServicesController < ApplicationController
 
+    #INDEX get '/' - @services = Service.all >> show all service instances aka "service menu"; redirects to 'services/all.erb'
+
+    #stretch goal:
+    #SHOW get 'services/:id' = find service by id (user clicks links from all.erb page), render 'services/show.erb' view with individual service instance details.
+
 end
+
+#views needed:
+# appointments_controller index redirects to '/services/all.erb'
+
+#stretch goal: '/services/show.erb' - show page for a specific service with all details of that instance: name, duration, price, (appointment_id - perhaps use to show IF they have this service currently booked)
